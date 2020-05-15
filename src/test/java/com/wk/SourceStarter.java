526990ddef9073679654417c1edc9f93a7c53d18
emperWang <544094478@qq.com>
2020-05-15 17:06:16 +0800

    add comment

--- src/test/java/com/wk/SourceStarter.java
+++ src/test/java/com/wk/SourceStarter.java
@@ -16,6 +16,7 @@ public class SourceStarter {
   public void sources() throws IOException {
     SqlSessionFactory sessionFactory;
     Reader reader = Resources.getResourceAsReader("MyBatis-config.xml");
+    // 创建sqlSessionFactory,在这里对xml文件进行了解析
     sessionFactory = new SqlSessionFactoryBuilder().build(reader);
     SqlSession sqlSession = sessionFactory.openSession();
     UserMapper mapper = sqlSession.getMapper(UserMapper.class);
